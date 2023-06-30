9213345791c1c5a0cc753af2d7e5f699eb7de9da
Jpdsf <jpdsafg@gmail.com>
2023-06-30 20:05:05 -0300

    Ajuste na formula

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ let resultado = document.querySelector('#resultado')
 
 function calcular(){
     let resposta = document.querySelector('#alinhar').value
+    console.log(`Resposta ${resposta}`)
     let questoes = document.querySelector('#questoes').value
-    let nota = (((((resposta / questoes) * 80) * 1.25) + 20) * 4) / 100
+    console.log(`Questoes ${questoes}`)
+    let pontuacaoEquiparada = (resposta / questoes) * 80
+    console.log(`Pontuaca ${pontuacaoEquiparada}`)
+    let aproveitamento = (pontuacaoEquiparada * 1.25) + 20
+    console.log(`Aproveitamento ${aproveitamento}`)
+    let nota = (aproveitamento * 4) / 100
+    console.log(nota)
     if (nota >= 4){
         nota = 4
     }
