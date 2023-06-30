20a6e9c734174f8f3c8d203bf67ffd64bda4b6e5
Jpdsf <jpdsafg@gmail.com>
2023-06-30 19:20:39 -0300

    Ajustes no desing

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let resultado = document.querySelector('#resultado')
 
 function calcular(){
     let resposta = document.querySelector('#alinhar').value
-    const nota = (((((resposta / 72) * 80) * 1.25) + 20) * 4) / 100
+    let questoes = document.querySelector('#questoes').value
+    let nota = (((((resposta / questoes) * 80) * 1.25) + 20) * 4) / 100
+    if (nota >= 4){
+        nota = 4
+    }
     resultado.innerHTML = `<br>Você tirou ${nota.toFixed(1)}`
 }
